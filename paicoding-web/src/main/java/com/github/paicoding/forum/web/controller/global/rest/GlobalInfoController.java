5dd571443e0ee24e50ce83e652c7c7ce7a164446
Vio <3273441974@qq.com>
2025-12-12 15:38:53 +0800

    初始化项目

---
+++ paicoding-web/src/main/java/com/github/paicoding/forum/web/controller/global/rest/GlobalInfoController.java
@@ -0,0 +1,27 @@
+package com.github.paicoding.forum.web.controller.global.rest;
+
+import com.github.paicoding.forum.web.global.vo.ResultVo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: tech-pai
+ * @description:
+ * @author: XuYifei
+ * @create: 2024-07-05
+ */
+
+@RestController
+@RequestMapping("api/global")
+public class GlobalInfoController {
+    /**
+     * 用于单独提供一个接口，用于前端获取全局信息
+     * @return
+     */
+    @GetMapping("info")
+    public ResultVo<String> getGlobalInfo() {
+        //todo 这里应该要提供全局信息的,暂时还没做
+        return ResultVo.ok("ok");
+    }
+}
---
+++ paicoding-web/src/main/java/com/github/paicoding/forum/web/controller/login/pwd/LoginRestController.java
@@ -0,0 +1,106 @@
+package com.github.paicoding.forum.web.controller.login.pwd;
+
+import com.github.paicoding.forum.api.model.context.ReqInfoContext;
+import com.github.paicoding.forum.api.model.vo.ResVo;
+import com.github.paicoding.forum.api.model.vo.constants.StatusEnum;
+import com.github.paicoding.forum.api.model.vo.login.UserNamePasswordReq;
+import com.github.paicoding.forum.api.model.vo.user.UserPwdLoginReq;
+import com.github.paicoding.forum.core.permission.Permission;
+import com.github.paicoding.forum.core.permission.UserRole;
+import com.github.paicoding.forum.core.util.SessionUtil;
+import com.github.paicoding.forum.service.user.service.LoginService;
+import com.github.paicoding.forum.web.controller.home.vo.LoginSuccessVo;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.apiguardian.api.API;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.Optional;
+
+/**
+ * 用户名 密码方式的登录/登出的入口
+ *
+ * @author XuYifei
+ * @date 2024-07-12
+ */
+@RestController
+@RequestMapping
+@Tag(name = "登录与注册", description = "账密模块")
+public class LoginRestController {
+    @Autowired
+    private LoginService loginService;
+
+    /**
+     * 用户名和密码登录
+     */
+    @PostMapping("/login/username")
+    public ResVo<Boolean> login(@RequestParam(name = "username") String username,
+                                @RequestParam(name = "password") String password,
+                                HttpServletResponse response) {
+        String session = loginService.loginByUserPwd(username, password);
+        if (StringUtils.isNotBlank(session)) {
+            // cookie中写入用户登录信息，用于身份识别
+            response.addCookie(SessionUtil.newCookie(LoginService.SESSION_KEY, session));
+            return ResVo.ok(true);
+        } else {
+            return ResVo.fail(StatusEnum.LOGIN_FAILED_MIXED, "用户名和密码登录异常，请稍后重试");
+        }
+    }
+
+    /**
+     * 用户名和密码登录
+     */
+    @PostMapping("/new/login/username")
+    public ResVo<LoginSuccessVo> loginByPassword(@RequestBody UserNamePasswordReq req,
+                                                 HttpServletResponse response) {
+        String session = loginService.loginByUserPwd(req.getUsername(), req.getPassword());
+        if (StringUtils.isNotBlank(session)) {
+            // cookie中写入用户登录信息，用于身份识别
+            Cookie cookie = SessionUtil.newCookie(LoginService.SESSION_KEY, session);
+            response.addCookie(cookie);
+            return ResVo.ok(new LoginSuccessVo(cookie.getValue()));
+        } else {
+            return ResVo.fail(StatusEnum.LOGIN_FAILED_MIXED, "用户名和密码登录异常，请稍后重试");
+        }
+    }
+
+    /**
+     * 注册
+     */
+    //todo 调整业务逻辑,以后不在注册界面绑定星球了
+    @PostMapping("/login/register")
+    public ResVo<Boolean> register(UserPwdLoginReq loginReq,
+                                   HttpServletResponse response) {
+        String session = loginService.registerByUserPwd(loginReq);
+        if (StringUtils.isNotBlank(session)) {
+            // cookie中写入用户登录信息，用于身份识别
+            response.addCookie(SessionUtil.newCookie(LoginService.SESSION_KEY, session));
+            return ResVo.ok(true);
+        } else {
+            return ResVo.fail(StatusEnum.LOGIN_FAILED_MIXED, "用户名和密码登录异常，请稍后重试");
+        }
+    }
+
+
+    @Permission(role = UserRole.LOGIN)
+    @RequestMapping("/logout")
+    public ResVo<Boolean> logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 释放会话
+//        request.getSession().invalidate();
+        Optional.ofNullable(ReqInfoContext.getReqInfo()).ifPresent(s -> loginService.logout(s.getSession()));
+        // 移除cookie
+        response.addCookie(SessionUtil.delCookie(LoginService.SESSION_KEY));
+        // 重定向到当前页面
+//        String referer = request.getHeader("Referer");
+//        if (StringUtils.isBlank(referer)) {
+//            referer = "/";
+//        }
+//        response.sendRedirect(referer);
+        return ResVo.ok(true);
+    }
+}
