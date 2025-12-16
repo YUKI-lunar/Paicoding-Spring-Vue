5db4148b89c4f9c621e9366fdd711ee14b2a8f70
Vio <3273441974@qq.com>
2025-12-16 19:23:55 +0800

    feat.增加跨域处理&设置samesite

--- paicoding-web/src/main/java/com/github/paicoding/forum/web/config/CorsConfig.java
+++ paicoding-web/src/main/java/com/github/paicoding/forum/web/config/CorsConfig.java
@@ -9,11 +9,16 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/admin/**")            // 只对 /admin 下的接口生效
-                .allowedOriginPatterns("*")        // 允许所有来源访问
-                .allowedMethods("*")               // 允许 GET, POST, PUT, DELETE 等
-                .allowedHeaders("*")               // 允许所有请求头
-                .allowCredentials(true)            // 允许携带 Cookie 或认证信息
-                .maxAge(3600);                     // 预检请求缓存时间，单位秒
+        registry.addMapping("/**")                // 匹配所有接口
+                .allowedOriginPatterns(
+                        "http://localhost:5173",  // 本地开发前端
+                        "http://127.0.0.1:5173",  // 本地开发前端
+                        "http://203.10.98.186"   // 你的公网 IP
+                )
+                .allowedMethods("*")            // 允许 GET, POST, PUT, DELETE 等
+                .allowedHeaders("*")            // 允许所有请求头
+                .allowCredentials(true)         // 允许携带 Cookie 或认证信息
+                .maxAge(3600);                  // 预检请求缓存时间，单位秒
     }
 }
+
